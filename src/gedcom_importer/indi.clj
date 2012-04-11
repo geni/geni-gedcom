bf757716a8f16e517608c48b825c19c8a2b47791
Anthony Grimes <i@raynes.me>
2012-04-11 18:09:49 -0500

    Add some parsing for BIRTH and dates.
    Needs cleaned up.

--- src/gedcom_importer/indi.clj
+++ src/gedcom_importer/indi.clj
@@ -1,5 +1,6 @@
 (ns gedcom-importer.indi
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [useful.utils :as utils]))
 
 (defmulti to-geni first)
 
@@ -15,6 +16,58 @@
      :middle_name (when middles (string/join " " middles))
      :last_name (when last-name (last (re-find #"/(.*)/" last-name)))
      :suffix suffix}))
+
+(defmethod to-geni "ADDR" [record]
+  (let [addr (-> record second first)
+        extract (fn [k] (-> k addr first :data))]
+    {:location {:country (extract "CTRY")
+                :state   (extract "STAE")
+                :city    (extract "CITY")}}))
+
+(defmethod to-geni "PLAC" [record]
+  (let [plac (-> record second first :data)]
+    {:location {:place plac}}))
+
+(letfn [(clean-date [date]
+          (string/replace
+            date
+            #"b\.?c\.?|a\.?d\.?|c\.?a?\.|circa|unknown|FROM|TO|BET|BTN|AFT|BEF|AND|about|ab|ABT|CAL|EST|INT|\(.*\)"
+            ""))
+
+        (date-to-map [on date & [reverse?]]
+          (zipmap (if reverse?
+                    [:year :month :day]
+                    [:day :month :year])
+                  (string/split date on)))
+
+        (lookup-month [month] month)
+
+        (parse-component [acc component]
+          (cond (re-find #"\d+" component)
+                (assoc acc
+                       (if (< 2 (count component)) :year :day)
+                       (Integer. component))
+                (re-find #"\W" component) acc
+                :else
+                (assoc acc :month (lookup-month component))))]
+
+  (defmethod to-geni "DATE" [record]
+    (let [plac (-> record second first :data)
+          date (clean-date plac)
+          approximate (re-find #"about|ab|ABT|CAL|EST|INT" plac)]
+      (cond (some #{\/} date)
+            {:date (date-to-map #"/" date)}
+            (re-find #"\d{1,2}-\d{1,2}-\d{4}" date)
+            {:date (date-to-map #"-" date)}
+            (re-find #"\d{4}-\d{1,2}-\d{1,2}" date)
+            {:date (date-to-map #"-" date :reverse)}
+            :else
+            (reduce parse-component
+                    {:circa (boolean approximate)}
+                    (string/split date #"\b"))))))
+
+(defmethod to-geni "BIRTH" [record]
+  ())
 
 (defmethod to-geni :default [_] nil)
 
