5c84b86dea47be9bf24fc7c160af4b8561149fc9
Anthony Grimes <i@raynes.me>
2012-04-16 13:01:40 -0700

    Upgrade to Clojure 1.4.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/geni/gedcom-importer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [geni-clj-sdk "0.1.2"]
                  [clj-gedcom "0.1.0-alpha1"]
                  [useful "0.8.0-alpha2"]])
