6f208c673dc68fe6c50d15fdb230c29b71495a14
Anthony Grimes <i@raynes.me>
2012-05-30 14:41:33 -0500

    Update clj-gedcom.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [geni-clj-sdk "0.1.3"]
-                 [clj-gedcom "0.1.0-alpha2"]
+                 [clj-gedcom "0.1.0-alpha3"]
                  [useful "0.8.0-alpha2"]
                  [noir "1.3.0-beta2"]]
   :main gedcom-importer.web.service)
