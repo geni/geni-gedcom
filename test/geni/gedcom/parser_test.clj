cfbfc8b48dd045419522c367e12e314aaac8c087
Anthony Grimes <i@raynes.me>
2012-05-31 18:31:12 -0500

    Add tests for union events.

--- test/geni/gedcom/parser_test.clj
+++ test/geni/gedcom/parser_test.clj
@@ -1,18 +1,23 @@
 (ns geni.gedcom.parser-test
   (:require [clojure.test :refer :all]
             [clj-gedcom.core :refer [parse-gedcom]]
+            [geni.gedcom.fam :as fam]
             [geni.gedcom.indi :as indi]))
 
 (def records (parse-gedcom (str (System/getProperty "user.dir") "/gedcoms/indi.ged")))
 
 (deftest event-test
   (let [person (indi/indi (records "@I0@"))
+        fam (fam/fam (records "@I0@"))
         event-data {:date {:year 1994, :day 2, :month 2, :circa false}
                     :location {:place_name "The Library"
                                :country "Westeros"
                                :state "Arkham"
                                :city "Metropolis"}}]
-    (apply = event-data (map person [:death :birth :burial :baptism]))))
+    (testing "Profile events are parsed properly."
+      (apply = event-data (map person [:death :birth :burial :baptism])))
+    (testing "Union events are parsed properly."
+      (apply = event-data (map fam [:marriage :divorce])))))
 
 (deftest name-test
   (let [person (indi/indi (records "@I0@"))]
@@ -28,3 +33,4 @@
     (is (apply = expected
                (map (partial get-date records)
                     ["@I2@" "@I3@" "@I4@" "@I5@" "@I6@"])))))
+
