f7e787eb82ac6deea81ecc69d8c20404f6b06842
Anthony Grimes <i@raynes.me>
2012-06-05 18:44:03 -0500

    Add a test that makes sure single-person unions aren't allowed.

--- test/geni/gedcom/import_test.clj
+++ test/geni/gedcom/import_test.clj
@@ -1,6 +1,6 @@
 (ns geni.gedcom.import_test
   (:require [clj-gedcom.core :refer [parse-gedcom]]
-            [geni.gedcom.common :refer [to-geni]]
+            [geni.gedcom.common :refer [to-geni profile-ids]]
             [useful.map :refer [map-vals merge-in]]
             [geni.gedcom.import :refer [walk-gedcom in-batches]]
             [clojure.test :refer :all]))
@@ -24,6 +24,11 @@
       (is (not (some (comp not unions) (keys (:profiles tree))))))
     (testing "Number of steps."
       (is (= 2 (count records))))))
+
+(deftest no-empty-unions
+  (let [tree (one-tree (walk-gedcom (gedcom "bad-union.ged") "@I1@"))]
+    (testing "No single-profile unions."
+      (is (every? #(> (count %) 1) (map profile-ids (vals (:unions tree))))))))
 
 (when (> 1 (count (.list (java.io.File. (str dir "/gedcoms/private")))))
   (deftest in-batches-test
