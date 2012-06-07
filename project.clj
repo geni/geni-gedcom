ea4864263510fb6ddb46c415101193d7062ad2cd
Anthony Grimes <i@raynes.me>
2012-06-06 20:07:35 -0500

    Allow for waring and configuring with a properties file.

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [geni-clj-sdk "0.1.4-alpha4"]
-                 [gedcom "0.1.0-alpha5"]
+                 [gedcom "0.1.0-alpha6"]
                  [useful "0.8.3-alpha2"]
                  [noir "1.3.0-beta2"]]
+  :plugins [[lein-ring "0.7.1"]]
+  :ring {:handler geni.gedcom.web/handler}
   :main geni.gedcom.web)
--- src/geni/gedcom/web.clj
+++ src/geni/gedcom/web.clj
@@ -1,10 +1,18 @@
 (ns geni.gedcom.web
   (:require [noir.core :refer [defpage defpartial]]
-            [noir.server :refer [start]]
+            [noir.server :refer [start gen-handler]]
             [noir.request :as request]
             [hiccup.form :as form]
             [geni.gedcom.import :refer [import-gedcom]]
-            [geni.core :as geni]))
+            [geni.core :as geni]
+            [clojure.java.io :refer [resource reader]])
+  (:import java.util.Properties))
+
+(defn read-properties []
+  (with-open [f (reader (resource "gedcom.properties"))]
+    (into {} (doto (Properties.) (.load f)))))
+
+(def config (read-properties))
 
 (defpartial form []
   [:body
@@ -21,11 +29,25 @@
      (form/submit-button "Submit")]])
 
 (defpage [:post "/import"] {:keys [gedcom indi token]}
-  (import-gedcom (:tempfile gedcom) indi token)
+  (prn token)
+  (binding [geni/*base* (config "url")
+            geni/*insecure* (Boolean/valueOf (config "insecure"))]
+    (import-gedcom (:tempfile gedcom) indi token))
   "DONE")
 
 (defpage "/" []
   (form))
 
+(def handler (gen-handler {:mode :prod}))
+
 (defn -main []
   (start 8080))
+
+;; war test code
+(comment
+  (import org.mortbay.jetty.Server
+          org.mortbay.jetty.webapp.WebAppContext)
+  
+  (doto (Server. 8080)
+    (.setHandler (WebAppContext. "target/importer.war" "/"))
+    .start))
