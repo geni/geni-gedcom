1a28c4759fd227444e41ea2fcccf0e9bfd9c1af4
Anthony Grimes <i@raynes.me>
2012-06-08 18:08:22 -0500

    Version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni-gedcom "0.0.1"
+(defproject geni-gedcom "0.0.2"
   :description "A GEDCOM to Geni importer."
   :url "http://github.com/geni/gedcom-importer"
   :license {:name "Eclipse Public License"
