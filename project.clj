2d36f698ab8eedaa8d7b21b64108ff4efcf828a2
Anthony Grimes <i@raynes.me>
2012-06-13 19:24:28 -0500

    Upgrade noir dep.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [geni-clj-sdk "0.1.4-alpha4"]
                  [gedcom "0.1.0-alpha6"]
                  [useful "0.8.3-alpha2"]
-                 [noir "1.3.0-beta2"]]
+                 [noir "1.3.0-beta8"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler geni.gedcom.web/handler
          :war-exclusions [#"gedcom.properties"]}
