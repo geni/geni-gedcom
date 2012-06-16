28dfb4ab679c2f5b735a7a20619b0e0af85e4fdf
Anthony Grimes <i@raynes.me>
2012-06-15 19:38:07 -0500

    Inc version and depend on gedcom alpha7.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject geni-gedcom "0.0.3"
+(defproject geni-gedcom "0.0.4"
   :description "A GEDCOM to Geni importer."
   :url "http://github.com/geni/gedcom-importer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [geni-clj-sdk "0.1.4-alpha4"]
-                 [gedcom "0.1.0-alpha6"]
+                 [gedcom "0.1.0-alpha7"]
                  [useful "0.8.3-alpha2"]
                  [noir "1.3.0-beta8"]]
   :plugins [[lein-ring "0.7.1"]]
