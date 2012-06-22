8f117e95aaf2a58c0a55b91be53358a181b8ff4a
Anthony Grimes <i@raynes.me>
2012-06-22 17:00:47 -0500

    Rewrite web service with compojure and get rid of old replaced interface stuff.

--- src/geni/gedcom/web/views/api.clj
+++ src/geni/gedcom/web/views/api.clj
@@ -3,11 +3,20 @@
             [geni.gedcom.web.common :refer [catch-exception-string]]
             [gedcom.core :refer [parse-gedcom]]
             [geni.gedcom.common :refer [to-geni]]
-            [noir.core :refer [defpage]]
-            [noir.response :refer [json]]))
+            [compojure.core :refer [defroutes POST]]
+            [ring.util.response :refer [response status content-type]]
+            [cheshire.core :refer [generate-string]]))
 
-(defpage [:post "/upload"] {:keys [gedcom token]}
-  (json
+(defn respond [processed]
+  (-> (generate-string processed)
+      response
+      (status (if (:error processed)
+                422
+                200))
+      (content-type "application/json")))
+
+(defn upload-ged [gedcom token]
+  (respond
    (let [results (catch-exception-string
                   (parse-gedcom (:tempfile gedcom) to-geni))]
      (if (map? results)
@@ -15,8 +24,8 @@
            {:names (names results)})
        {:error results}))))
 
-(defpage [:post "/import"] {:keys [id token]}
-  (json
+(defn import-ged [id token]
+  (respond
    (if-let [records (:records (get @cache token))]
      (let [results (import-records records id token)]
        (swap! cache dissoc token)
@@ -24,3 +33,9 @@
          {:profiles_imported (count-profiles results)}
          {:error results}))
      {:error "No records associated with this token."})))
+
+(defroutes api-routes
+  (POST "/upload" {{:keys [gedcom token]} :params}
+    (upload-ged gedcom token))
+  (POST "/import" {{:keys [id token]} :params}
+    (import-ged id token)))
