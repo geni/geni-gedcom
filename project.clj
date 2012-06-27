a0bb2c89527870ff3aa8ad14afe6355f07293ee8
Anthony Grimes <i@raynes.me>
2012-06-27 17:18:50 -0500

    Upgrade ring-cors to 0.0.6 and inc importer version to 0.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni-gedcom "0.0.10"
+(defproject geni-gedcom "0.0.11"
   :description "A GEDCOM to Geni importer."
   :url "http://github.com/geni/geni-gedcom"
   :license {:name "Eclipse Public License - v 1.0"
@@ -10,7 +10,7 @@
                  [useful "0.8.3-alpha2"]
                  [compojure "1.1.0"]
                  [lib-noir "0.1.1"]
-                 [flatland/ring-cors "0.0.5"]]
+                 [flatland/ring-cors "0.0.6"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler geni.gedcom.web.server/handler
          :war-exclusions [#"gedcom.properties"]}
