5768d0661b03664f4eba4614cb2692ba056525eb
Anthony Grimes <i@raynes.me>
2012-07-10 18:05:19 -0500

    Clean up view, moving logic to model. Change to work with import-gedcom's new behavior.
    The import API call returns immediately, returning the number of profiles.
    As the gedcom is imported, update the progress number in the cache and add
    a /progress endpoint to view the progress. When finished, set :status to done.

--- src/geni/gedcom/web/models/api.clj
+++ src/geni/gedcom/web/models/api.clj
@@ -1,26 +1,40 @@
 (ns geni.gedcom.web.models.api
   (:require [geni.gedcom.web.common :refer [catch-exception-string config]]
-            [geni.gedcom.import :refer [import-gedcom]]
+            [geni.gedcom.import :as import]
             [geni.gedcom.common :refer [to-geni]]
             [geni.core :as geni]
-            [clojure.string :refer [join]]))
+            [gedcom.core :refer [parse-gedcom]]
+            [clojure.string :refer [join]]
+            [clojure.tools.logging :refer [info error]]))
 
 (def cache (atom {:id 0}))
 
 (defn cache-records
   "Cache records that may be imported in the near future."
-  [cache records token]
+  [cache records record-count token]
   (let [id (inc (:id cache))]
     (assoc cache
-      token {:id id, :records records}
+      token {:id id
+             :records records
+             :profile-count record-count
+             :progress 0
+             :status :waiting}
       :id id)))
+
+(defn update-status [token v]
+  (swap! cache assoc-in [token :status] v))
+
+(defn check-progress [token]
+  (let [cached (get @cache token)]
+    (merge {:profile_count (:profile-count cached)}
+           (select-keys cached [:progress :status]))))
 
 (defn remove-records
   "If the cached id is the same as id, remove the records
    associated with token."
   [cache token id]
   (if (= id (get-in cache [token :id]))
-    (dissoc cache token)
+    (update-in cache [token] dissoc :records)
     cache))
 
 (defn future-clean
@@ -48,15 +62,50 @@
           {}
           records))
 
-;; Waiting on Scott to add count keys to import_tree's results.
-(defn count-profiles
-  "Count the number of profiles returned by the Geni API."
-  [results]
-  (count (filter #(.startsWith (val %) "profile") results)))
+(defn prepare-gedcom
+  "Parses an uploaded gedcom, stores the parsed records in the cache,
+   sets up a thread to to clear the records from the cache after 5
+   minutes, and returns either a map with an :error key if an exception
+   occurred or a map with a :names key keyed to a map of GEDCOM labels
+   to names."
+  [gedcom token]
+  (let [results (catch-exception-string (str "Error parsing gedcom for " token)
+                  (parse-gedcom (:tempfile gedcom) to-geni))]
+    (if (map? results)
+      (let [names (names results)]
+        (future-clean token (:id (swap! cache cache-records results (count names) token)))
+        (info (str "Successfully parsed gedcom records for " token))
+        {:names names})
+      {:error results})))
 
 (defn import-records
   "Call import-gedcom respecting settings in gedcom.properties."
-  [records id token]
-  (binding [geni/*base* (config "url")
-            geni/*insecure* (Boolean/valueOf (config "insecure"))]
-    (catch-exception-string (import-gedcom records id token))))
+  [records id token]  
+  (.start
+   (Thread.
+    (fn []
+      (binding [geni/*base* (config "url")
+                geni/*insecure* (Boolean/valueOf (config "insecure"))]
+        (let [result-seq (import/import-gedcom records id token)]
+          (info (str "Importing profiles for " token))
+          (update-status token :importing)
+          (doseq [[progress x] result-seq]
+            (swap! cache assoc-in [token :progress] progress))
+          (update-status token :done)
+          (info (str "Imported profiles for " token)))))))
+  {:profile-count (get-in @cache [token :profile-count])})
+
+(defn import-gedcom
+  "Initiates an import with records previously cached against token.
+   The import begins with id in the GEDCOM and walks out from there.
+   If an error occurs, returns a map with an :error key, otherwise
+   returns the number of profiles to be imported. The :progress key
+   in the map associated with token in the cache will be updated at
+   every interation of the import."
+  [id token]
+  (if-let [records (get-in @cache [token :records])]
+    (do (swap! cache update-in [token] dissoc :records)
+        (import-records records id token))
+    (let [message (str "No records associated with token " token)]
+      (error message)
+      {:error message})))
--- src/geni/gedcom/web/views/api.clj
+++ src/geni/gedcom/web/views/api.clj
@@ -1,11 +1,8 @@
 (ns geni.gedcom.web.views.api
-  (:require [geni.gedcom.web.models.api :refer [future-clean import-records count-profiles cache names cache-records]]
-            [geni.gedcom.web.common :refer [catch-exception-string]]
-            [gedcom.core :refer [parse-gedcom]]
-            [geni.gedcom.common :refer [to-geni]]
-            [compojure.core :refer [defroutes POST]] 
+  (:require [geni.gedcom.web.models.api :refer [prepare-gedcom import-gedcom check-progress]]
+            [compojure.core :refer [defroutes POST GET]] 
             [noir.response :refer [json status]]
-            [clojure.tools.logging :refer [info error]]))
+            [clojure.tools.logging :refer [info]]))
 
 (defn respond [processed]
   (->> (json processed)
@@ -15,32 +12,19 @@
 
 (defn upload-ged [gedcom token]
   (info (str "Starting upload for " token))
-  (respond
-   (let [results (catch-exception-string (str "Error parsing gedcom for " token)
-                   (parse-gedcom (:tempfile gedcom) to-geni))]
-     (if (map? results)
-       (do (future-clean token (:id (swap! cache cache-records results token)))
-           (info (str "Successfully parsed gedcom records for " token))
-           {:names (names results)})
-       {:error results}))))
+  (respond (prepare-gedcom gedcom token)))
 
 (defn import-ged [id token]
   (info (str "Importing records for " token))
-  (respond
-   (if-let [records (:records (get @cache token))]
-     (let [results (import-records records id token)]
-       (swap! cache dissoc token)
-       (if (map? results)
-         (let [count (count-profiles results)]
-           (info (str "Successfully imported " count " profiles for " token))
-           {:profiles_imported count})
-         {:error results}))
-     (let [message (str "No records associated with token " token)]
-       (error message)
-       {:error message}))))
+  (respond (import-gedcom id token)))
+
+(defn progress [token]
+  (respond (check-progress token)))
 
 (defroutes api-routes
   (POST "/upload" {{:keys [gedcom token]} :params}
     (upload-ged gedcom token))
   (POST "/import" {{:keys [id token]} :params}
-    (import-ged id token)))
+    (import-ged id token))
+  (GET "/progress" {{:keys [token]} :params}
+    (progress token)))
