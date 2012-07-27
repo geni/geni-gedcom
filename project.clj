1d34f10a5a3b082588af5681210310c642ed391e
lance bradley <l@ncebradley.org>
2012-07-27 14:08:49 -0700

    0.0.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni-gedcom "0.0.14"
+(defproject geni-gedcom "0.0.15"
   :description "A GEDCOM to Geni importer."
   :url "http://github.com/geni/geni-gedcom"
   :license {:name "Eclipse Public License - v 1.0"
